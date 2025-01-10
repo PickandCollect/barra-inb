8796c0e2a1331d4eccd745460e18723af0525b43
PickandCollect <perez85alex08@gmail.com>
2025-01-10 22:29:50 +0000

    10/01/2025

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dac7165d453b6cc5fb9b84aa4a0136664c176215',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dac7165d453b6cc5fb9b84aa4a0136664c176215',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
